fd3c186771e02c73260ad5b8661ee0aca484b105
candrewdb9 <47709531+candrewdb9@users.noreply.github.com>
2021-05-12 13:24:47 +1000

    Add files via upload
    C stuff

---
+++ XC8Application1/XC8Application1/main.c
@@ -0,0 +1,26 @@
+#define F_CPU 1000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+void main()
+{
+	//FOR TIMER1
+	TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM
+	TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM)
+	ICR1=312;  //fPWM=50Hz
+	DDRB = 1<<DDB5;   //PWM Pins as Output
+	//OCR1A=30;  //90 degree
+	//_delay_ms(3000);
+	//OCR1A=15;
+	//_delay_ms(3000);
+	while(1)
+	{
+		
+		OCR1A=15;
+		_delay_ms(3000);
+		OCR1A=18;
+		_delay_ms(3000);
+		
+	}
+}
